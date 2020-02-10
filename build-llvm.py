9c69773383ed973c920170f513b2b791f08cae1f
Danny Lin <danny@kdrag0n.dev>
2020-02-09 18:33:16 -0800

    tc-build: add our build script
    Signed-off-by: alk3pInjection <webmaster@raspii.tech>

--- build-llvm.py
+++ build-llvm.py
@@ -609,7 +609,7 @@ def fetch_llvm_binutils(root_folder, llvm_folder, update, shallow, ref):
                 extra_args += ("--no-single-branch", )
         subprocess.run([
             "git", "clone", *extra_args,
-            "https://github.com/llvm/llvm-project",
+            "https://mirrors.nju.edu.cn/git/llvm-project.git",
             llvm_folder.as_posix()
         ],
                        check=True)
